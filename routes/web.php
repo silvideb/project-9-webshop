08dbaf2d6a48e987b6e808d6b2964a3c21ae1dcc
Mona Lisa <mona@Lisa.com>
2025-03-31 11:02:04 +0200

    coupon index, create, edit en delete zijn gemaakt(nog niet gelinkt aan admin rol)

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use App\Http\Controllers\CouponController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/cart', function (Request $request) {
+    $cart = session()->get('cart', []);
+    return view('cart', compact('cart'));
+});
+Route::get('/coupons', [CouponController::class, 'index'])->name('coupons.index');
+    Route::get('/coupons/create', [CouponController::class, 'create'])->name('coupons.create');
+    Route::post('/coupons', [CouponController::class, 'store'])->name('coupons.store');
+    Route::resource('coupons', CouponController::class);
