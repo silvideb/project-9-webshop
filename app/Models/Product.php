c3183ec3bb29392516e9ba6c383091992ad76043
silvio de boer <191617@novacollege.nl>
2025-04-01 09:27:07 +0200

    controllers en moddel

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,7 +9,7 @@ class Product extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['name', 'description', 'price'];
+    protected $fillable = ['name', 'description', 'price', 'stock'];
 
     public function variants()
     {
